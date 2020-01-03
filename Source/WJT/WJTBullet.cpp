c4a72570eeb4d9cfdebc7caacef4af49a09d286b
BeHindDark <wongt8969@gmail.com>
2020-01-03 22:57:42 +0900

    삽질

---
+++ Source/WJT/WJTBullet.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WJTBullet.h"
+
+// Sets default values
+AWJTBullet::AWJTBullet()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWJTBullet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWJTBullet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
