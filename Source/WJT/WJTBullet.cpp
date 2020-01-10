784a2df23cb416d94fbb96f2461a83843ecd0fec
BeHindDark <wongt8969@gmail.com>
2020-01-10 18:27:34 +0900

    ASDF

--- Source/WJT/WJTBullet.cpp
+++ Source/WJT/WJTBullet.cpp
@@ -9,13 +9,24 @@ AWJTBullet::AWJTBullet()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
 
+	RootComponent = Mesh;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> BULLET_MESH(TEXT("/Game/Mech_Constructor_Spiders/Meshes/Projectile_Bullet.Projectile_Bullet"));
+	if (BULLET_MESH.Succeeded()) {
+		Mesh->SetStaticMesh(BULLET_MESH.Object);
+	}
+
+	Mesh->AddRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
 }
 
 // Called when the game starts or when spawned
 void AWJTBullet::BeginPlay()
 {
 	Super::BeginPlay();
+		
 	
 }
 
