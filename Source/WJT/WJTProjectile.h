090c4e748b2bc56d2d4d43531a4d985ee7dd29c3
BeHindDark <wongt8969@gmail.com>
2020-01-17 22:19:32 +0900

    Ah

---
+++ Source/WJT/WJTProjectile.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "WJTWeapon_Projectile.h"
+#include "WJTProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WJT_API AWJTProjectile : public AWJTWeapon_Projectile
+{
+	GENERATED_BODY()
+	
+};
